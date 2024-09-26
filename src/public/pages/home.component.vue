3e16260745bd65f2006dfd01c00d749dce8e15f1
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-09-26 11:28:03 -0500

    feat(public): add home component.

---
+++ src/public/pages/home.component.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "home-view"
+}
+</script>
+
+<template>
+  <main class="home">
+    <div class="home__container">
+
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
