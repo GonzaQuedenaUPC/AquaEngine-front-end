641dde0232f4c4cbbf983b42818668859890910f
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-09-26 18:08:45 -0500

    style(public): add responsive design to toolbar.

--- src/public/components/toolbar.component.vue
+++ src/public/components/toolbar.component.vue
@@ -12,54 +12,168 @@ export default {
 </script>
 
 <template>
-  <pv-toolbar class="toolbar">
-    <template #start>
-      <div class="toolbar__logo">
-        <a href="#">
-          <img src="../../assets/images/aqua-engine-logo.svg" alt="aqua-engine-logo">
-        </a>
-      </div>
-    </template>
+  <nav class="nav">
+    <pv-toolbar class="toolbar">
+      <template #start>
+        <div class="toolbar__logo">
+          <a href="#">
+            <img src="../../assets/images/aqua-engine-logo.svg" alt="aqua-engine-logo">
+          </a>
+        </div>
+      </template>
 
-    <template #end>
-      <div class="toolbar__user">
-        <a href="" class="user__info">
-          <img src="../../assets/images/user.svg" alt="user-icon" class="user__logo"/>
-          <span class="user__username">{{ user.username }}</span>
-        </a>
-      </div>
-    </template>
+      <template #end>
 
-  </pv-toolbar>
+        <ul class="dropdown__main">
+          <li class="dropdown__list dropdown__user--main">
+            <a href="#" class="dropdown__link">
+              <img src="../../assets/images/user.svg" class="nav__icon" alt="user-icon"/>
+              <span class="dropdown__span dropdown__span--user">{{ user.username }}</span>
+            </a>
+          </li>
+        </ul>
+
+        <a href="#menu" class="nav__menu">
+          <img src="../../assets/images/menu.svg" class="nav__icon" alt="menu-icon"/>
+        </a>
+        <a href="#" class="nav__menu nav__menu--second">
+          <img src="../../assets/images/close.svg" class="nav__icon" alt="menu-icon"/>
+        </a>
+
+        <ul class="dropdown__menu" id="menu">
+          <li class="dropdown__list dropdown__user--menu">
+            <a href="#" class="dropdown__link">
+              <img src="../../assets/images/user.svg" class="nav__icon nav__icon--user" alt="user-icon"/>
+              <span class="dropdown__span dropdown__span--user">{{ user.username }}</span>
+            </a>
+          </li>
+
+          <li class="dropdown__list dropdown__list--close">
+            <a href="#" class="dropdown__link">
+              <img src="../../assets/images/user-close.svg" class="nav__icon" alt="user-icon"/>
+              <span class="dropdown__span dropdown__span--menu">Close Profile</span>
+            </a>
+          </li>
+        </ul>
+
+      </template>
+
+    </pv-toolbar>
+  </nav>
 </template>
 
 <style scoped>
+
+.nav {
+  margin: -3px;
+}
 
 .toolbar {
   width: 100%;
   padding: 15px;
   background-color: var(--primary-color);
+
+  --img1: scale(1);
+  --img2: scale(0);
+
+  overflow: hidden;
+}
+
+.toolbar:has(.dropdown__menu:target) {
+  --img1: scale(0);
+  --img2: scale(1);
+
+  --clip: inset(15.5% 0 0 0);
+}
+
+.nav__menu {
+  position: relative;
+  display: inline-block;
+  margin-right: 2rem;
+
+  z-index: 10;
+  transform: var(--img1);
+}
+
+.nav__menu--second {
+  position: absolute;
+  right: 5px;
+  transform: var(--img2);
+}
+
+.nav__icon {
+  width: 30px;
+  display: inline-block;
+}
+
+
+.dropdown__main {
+  display: none;
+}
+
+.dropdown__menu {
+  position: absolute;
+
+  background-color: var(--primary-color);
+  width: 50%;
+  max-width: 300px;
+
+  right: 0;
+  top: 0;
+  bottom: 0;
+
+  display: grid;
+  align-content: start;
+  gap: 1rem;
+
+  padding: 155px 0 0 30px;
+
+  clip-path: var(--clip, inset(15% 0 0 100%));
+  transition: clip-path .5s;
+}
+
+.dropdown__list {
+  list-style: none;
+}
+
+.dropdown__link,
+.toolbar {
+
+  display: flex;
+  gap: .6rem;
+  align-items: center;
+
+  color: var(--white);
+  padding: 1em .7em;
+  text-decoration: none;
+}
+
+.dropdown__span--user,
+.dropdown__span--menu {
+  margin-left: .5rem;
+  letter-spacing: 2px;
+}
+
+.dropdown__span--user {
+  color: var(--black);
+  font-weight: 600;
+  font-size: 20px;
 }
 
 .toolbar__logo {
   margin-left: 2rem;
 }
 
-.user__info {
-  display: flex;
-  margin-right: 2rem;
-}
+@media (min-width: 769px) {
+  .dropdown__main {
+    display: inline-block;
+    margin-right: 3rem;
+  }
 
-.user__logo {
-  width: 24px;
-  height: 24px;
-  margin-right: 1.2rem;
-}
+  .dropdown__user--menu {
+    display: none;
+  }
 
-.user__username {
-  color: var(--black); /* Color del texto */
-  font-weight: 700;
-  font-size: 1.2rem; /* Tamaño de la fuente del usuario */
 }
 
 </style>
