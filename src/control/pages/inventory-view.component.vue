487e52b501255ddad226b1c39ec8953d03800f01
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-09-26 22:36:41 -0500

    feat(control): add inventory management and view component.

---
+++ src/control/pages/inventory-view.component.vue
@@ -0,0 +1,31 @@
+<script>
+import InventoryManagementComponent from "../components/inventory-management.component.vue";
+
+export default {
+  name: "inventory-view",
+
+  components: {
+    InventoryManagementComponent
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="content">
+      <div class="inventory">
+        <h1 class="inventory__title">Inventory</h1>
+        <p class="inventory__info">Track the fluctuation of your products</p>
+        <inventory-management-component/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.inventory__info {
+  margin: 10px 0;
+}
+
+</style>
