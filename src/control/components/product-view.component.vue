e0986129b44fb0766d6b2e7ae0f63cb8aa35425f
OmBRz <omarbrrcl@gmail.com>
2024-09-27 17:45:00 -0500

    feat(control): product-view component added.

---
+++ src/control/components/product-view.component.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: "product-view",
+
+  props: {
+    product: {
+      type: Object,
+      required: true
+    },
+    visible: {
+      type: Boolean,
+      required: true
+    }
+  },
+
+  methods: {
+    closeDialog() {
+      this.$emit('update:visible', false);
+    }
+  }
+}
+</script>
+
+<template>
+  <pv-dialog :header="product.name" :visible="visible" modal @hide="closeDialog">
+    <div>
+      <p><strong>ID:</strong> {{ product.productId }}</p>
+      <p><strong>Name:</strong> {{ product.name }}</p>
+      <p><strong>Price:</strong> {{ product.price }}</p>
+      <p><strong>Stock:</strong> {{ product.stock }}</p>
+      <img :src="product.urlToImage" alt="Product Image" />
+    </div>
+    <template #footer>
+      <pv-button label="Close" @click="closeDialog"/>
+    </template>
+  </pv-dialog>
+</template>
+
+<style scoped>
+/* Add any necessary styles here */
+</style>
