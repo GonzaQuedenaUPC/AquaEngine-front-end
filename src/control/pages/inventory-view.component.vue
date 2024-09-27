a6d15293e68bdc6822f73678a54b0ce06c9faac3
OmBRz <omarbrrcl@gmail.com>
2024-09-27 17:46:43 -0500

    feat(control): added a method to inventory view component.

--- src/control/pages/inventory-view.component.vue
+++ src/control/pages/inventory-view.component.vue
@@ -1,11 +1,13 @@
 <script>
 import InventoryManagementComponent from "../components/inventory-management.component.vue";
+import ProductView from "../components/product-view.component.vue";
 
 export default {
   name: "inventory-view",
 
   components: {
-    InventoryManagementComponent
+    InventoryManagementComponent,
+    ProductView
   },
 
   props: {
@@ -18,12 +20,18 @@ export default {
   data() {
     return {
       localInventory: [],
+      selectedProduct: null,
+      isDialogVisible: false
     }
   },
 
   methods: {
     _emitProfileLoaded(profileSaved) {
       this.$emit('profile-loaded', profileSaved);
+    },
+    showProductView(product) {
+      this.selectedProduct = product;
+      this.isDialogVisible = true;
     }
   },
 
@@ -45,17 +53,16 @@ export default {
       <div class="inventory">
         <h1 class="inventory__title">Inventory</h1>
         <p class="inventory__info">Track the fluctuation of your products</p>
-        <inventory-management-component :inventory="this.localInventory"/>
+        <inventory-management-component :inventory="localInventory" @view-product="showProductView"/>
       </div>
     </div>
+    <product-view v-if="selectedProduct" :product="selectedProduct" :visible.sync="isDialogVisible"/>
   </div>
 </template>
 
 <style scoped>
-
 .inventory__info {
   margin-top: 10px;
   margin-bottom: 25px;
 }
-
 </style>
