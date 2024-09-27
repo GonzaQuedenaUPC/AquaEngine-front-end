4488ce75ae8f3184b1b7eeb3b1cf0795c518d415
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-09-26 23:23:54 -0500

    feat(control): add profile service.

---
+++ src/control/services/profile.service.js
@@ -0,0 +1,12 @@
+import http from '../../shared/http-common.js';
+
+export class ProfileService {
+
+    _resourceEndpoint = '/profiles';
+
+    getAll() {
+        console.log(http.defaults.baseURL);
+        console.log(this._resourceEndpoint);
+        return http.get(this._resourceEndpoint);
+    }
+}
