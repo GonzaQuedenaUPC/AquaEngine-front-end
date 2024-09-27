65b75495bdcd3726c845ae17e9654eb93ee8c92a
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-09-26 22:37:40 -0500

    feat(router): add inventory route to routes.

--- src/router/index.js
+++ src/router/index.js
@@ -5,19 +5,23 @@ import {createRouter, createWebHistory} from "vue-router";
 import HomeComponent from "../public/pages/home.component.vue";
 import AboutComponent from "../public/pages/about.component.vue";
 import CategoryManagementComponent from "../publishing/pages/category-management.component.vue";
-
 */
+
+import InventoryViewComponent from '../control/pages/inventory-view.component.vue';
+
+const routes = [
+    { path: '/inventory', name: 'inventory', component: InventoryViewComponent, meta: { title: 'Inventory'}},
+]
+
 const router = createRouter({
     history: createWebHistory(),
-    routes: [
-        //{ path: '/home',                    name: 'home', component: HomeComponent, meta: { title: 'Home'}},
-        //{ path: '/',                        redirect: '/home'}
-    ]
+    routes,
 });
 
 router.beforeEach((to, from, next) => {
-    let baseTitle = '';
+    let baseTitle = 'AquaEngine';
     document.title = `${baseTitle} | ${to.meta['title']}`;
     next();
 })
+
 export default router;
