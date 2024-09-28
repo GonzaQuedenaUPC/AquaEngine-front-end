7be72c4dbe2c7b73e89027eb84869aace7ee033c
Pedro Guia <u202212010@upc.edu.pe>
2024-09-27 19:41:02 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
-
 import {createRouter, createWebHistory} from "vue-router";
+import OrderDetailComponent from "../planning/pages/order-detail.component.vue";
+import OrderingMachineryComponent from "../planning/pages/ordering-machinery.component.vue";
 
 /* Import your components here
 import HomeComponent from "../public/pages/home.component.vue";
@@ -10,8 +11,8 @@ import CategoryManagementComponent from "../publishing/pages/category-management
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        //{ path: '/home',                    name: 'home', component: HomeComponent, meta: { title: 'Home'}},
-        //{ path: '/',                        redirect: '/home'}
+        { path: '/', name: 'OrderingMachinery', component: OrderingMachineryComponent },
+        { path: 'order-detail', name: 'OrderDetail', component: OrderDetailComponent}
     ]
 });
 
