dfdaa4782d5582ed472a57ce2878858a2c056933
teocchiii <u202211212@upc.edu.pe>
2024-09-27 20:40:12 -0500

    refactor(router): routing module are optimised now

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,6 @@ import MaintenanceViewComponent from "../analytics/pages/maintenance-view.compon
 
 const routes = [
     { path: '/inventory', name: 'inventory', component: InventoryViewComponent, meta: { title: 'Inventory'}},
-    {path:'/monitoring', name:'monitoring', component: MonitoringViewComponent, meta: {title: 'Monitoring'}},
     { path: '/home', name: 'home', component: HomeViewComponent, meta: { title: 'Home'}},
     {path: '/monitoring/:id/maintenance', name: 'maintenance-view', component: MaintenanceViewComponent, props: true},
     { path: '/', redirect: '/home'}
