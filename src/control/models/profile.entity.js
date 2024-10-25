e01a81386e4da8988ca6ed6fe7826b1898a9219e
Pedro Guia <u202212010@upc.edu.pe>
2024-10-25 12:07:42 -0500

    feat(control): add inventory system implementation.

---
+++ src/control/models/profile.entity.js
@@ -0,0 +1,33 @@
+class Profile {
+
+    constructor({id='', name='', email='',
+                    ruc='', inventory=[]}) {
+        this._id = id;
+        this._name = name;
+        this._email = email;
+        this._ruc = ruc;
+        this._inventory = inventory;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    get ruc() {
+        return this._ruc;
+    }
+
+    get inventory() {
+        return this._inventory;
+    }
+}
+
+export default Profile;
