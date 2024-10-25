3eb0ebfe57a4352815116d6b801db1c4837f5c0a
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-25 00:29:09 -0500

    feat(control): add inventory form pop up component implementation.

--- src/control/components/inventory-management.component.vue
+++ src/control/components/inventory-management.component.vue
@@ -1,4 +1,5 @@
 <script>
+
 export default {
   name: "inventory-management",
 
@@ -22,15 +23,22 @@ export default {
     emitData(item) {
       this._emitPopUpState();
       this._emitItemSelected(item);
-    }
+    },
+
+    emitPopUpFormState() {
+      this.$emit('pop-up-form-state', true);
+    },
+
   }
 }
 </script>
 
 <template>
-  <div class="container w-full">
-    <div class="w-full flex justify-center">
-      <pv-data-table :value="inventory" paginator :rows="5" tableStyle="min-width: 50rem" class="w-full md:w-auto md:max-w-[90%] px-4 lg:p-0 mx-auto">
+  <div class="inventory container w-full">
+    <div class="inventory__content w-full flex justify-center flex-column gap-3">
+      <pv-data-table :value="inventory" paginator :rows="5" tableStyle="min-width: 50rem" class="inventory__table
+        w-full md:w-auto md:max-w-[90%] px-4 lg:p-0 mx-auto">
+
         <pv-column class="w-[10%] md:w-[25%]" field="productId" header="ID"></pv-column>
         <pv-column class="w-[10%] md:w-[25%]" field="name" header="Name"></pv-column>
         <pv-column class="w-[10%] md:w-[25%]" field="stock" header="Stock"></pv-column>
@@ -41,11 +49,14 @@ export default {
           </template>
         </pv-column>
       </pv-data-table>
+
+      <div class="inventory__footer w-full flex justify-end">
+        <pv-button label="Insert" style="background-color: #8298E7; color: #000; border: none;
+          min-width: 120px; height: 45px" class="inventory__btn" @click="emitPopUpFormState"/>
+      </div>
     </div>
   </div>
 </template>
-
-
 
 <style scoped>
 
