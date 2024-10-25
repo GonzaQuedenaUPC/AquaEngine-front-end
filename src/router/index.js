c2ed38fe9fdcdad27db6f802ad8f7d596cc5acae
Pedro Guia <u202212010@upc.edu.pe>
2024-10-25 13:34:35 -0500

    fix(ordering-machinery): the ordering machinery data are now cards in order to see the data as planed.

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,34 @@
-import {createRouter, createWebHistory} from "vue-router";
-import OrderDetailComponent from "../planning/pages/order-detail.component.vue";
-import OrderingMachineryComponent from "../planning/pages/ordering-machinery.component.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 /* Import your components here
-import HomeComponent from "../public/pages/home.component.vue";
 import AboutComponent from "../public/pages/about.component.vue";
 import CategoryManagementComponent from "../publishing/pages/category-management.component.vue";
-
 */
+
+import HomeViewComponent from "../public/pages/home.component.vue";
+import InventoryViewComponent from '../control/pages/inventory-view.component.vue';
+import MonitoringViewComponent from "../analytics/pages/monitoring-view.component.vue";
+import MaintenanceViewComponent from "../analytics/pages/maintenance-view.component.vue";
+import OrderingMachineryViewComponent from "../planning/pages/ordering-machinery-view.component.vue";
+
+const routes = [
+    { path: '/inventory', name: 'inventory', component: InventoryViewComponent, meta: { title: 'Inventory'}},
+    {path:'/monitoring', name:'monitoring', component: MonitoringViewComponent, meta: {title: 'Monitoring'}},
+    { path:'/ordering-machinery', mame:"ordering", component: OrderingMachineryViewComponent, meta: {title: 'Ordering Machinery'}},
+    { path: '/home', name: 'home', component: HomeViewComponent, meta: { title: 'Home'}},
+    {path: '/monitoring/:id/maintenance', name: 'maintenance-view', component: MaintenanceViewComponent, props: true},
+    { path: '/', redirect: '/home'}
+]
+
 const router = createRouter({
     history: createWebHistory(),
-    routes: [
-        { path: '/', name: 'OrderingMachinery', component: OrderingMachineryComponent },
-        { path: 'order-detail', name: 'OrderDetail', component: OrderDetailComponent}
-    ]
+    routes,
 });
 
 router.beforeEach((to, from, next) => {
-    let baseTitle = '';
+    let baseTitle = 'AquaEngine';
     document.title = `${baseTitle} | ${to.meta['title']}`;
     next();
-})
+});
+
 export default router;
