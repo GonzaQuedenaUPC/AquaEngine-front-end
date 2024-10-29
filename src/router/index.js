c222f93aa786426bba612aae4b810a7ecbadc587
Mitawarmi2000 <u202214130@upc.edu.pe>
2024-10-29 11:28:02 -0500

    feat(router):  added invoice routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-/* Import your components here
-import AboutComponent from "../public/pages/about.component.vue";
-import CategoryManagementComponent from "../publishing/pages/category-management.component.vue";
-*/
-
 import HomeViewComponent from "../public/pages/home.component.vue";
-import InventoryViewComponent from '../control/pages/inventory-view.component.vue';
+import InventoryViewComponent from "../control/pages/inventory-view.component.vue";
 import MonitoringViewComponent from "../analytics/pages/monitoring-view.component.vue";
 import MaintenanceViewComponent from "../analytics/pages/maintenance-view.component.vue";
+import InvoicingViewComponent from "../invoicing/components/Invoicing.component.vue";
 
 const routes = [
-    { path: '/inventory', name: 'inventory', component: InventoryViewComponent, meta: { title: 'Inventory'}},
-    {path:'/monitoring', name:'monitoring', component: MonitoringViewComponent, meta: {title: 'Monitoring'}},
-    { path: '/home', name: 'home', component: HomeViewComponent, meta: { title: 'Home'}},
-    {path: '/monitoring/:id/maintenance', name: 'maintenance-view', component: MaintenanceViewComponent, props: true},
-    { path: '/', redirect: '/home'}
-]
+    { path: '/inventory', name: 'inventory', component: InventoryViewComponent, meta: { title: 'Inventory' }},
+    { path: '/monitoring', name: 'monitoring', component: MonitoringViewComponent, meta: { title: 'Monitoring' }},
+    { path: '/home', name: 'home', component: HomeViewComponent, meta: { title: 'Home' }},
+    { path: '/monitoring/:id/maintenance', name: 'maintenance-view', component: MaintenanceViewComponent, props: true },
+    { path: '/invoicing', name: 'invoicing', component: InvoicingViewComponent, meta: { title: 'Invoicing' }},
+    { path: '/', redirect: '/home' }
+];
 
 const router = createRouter({
     history: createWebHistory(),
@@ -25,7 +22,7 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
     let baseTitle = 'AquaEngine';
-    document.title = `${baseTitle} | ${to.meta['title']}`;
+    document.title = `${baseTitle} | ${to.meta.title}`;
     next();
 });
 
