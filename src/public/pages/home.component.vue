2309261ac1cd0275e384e9699d7cad9724b269cb
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-30 15:58:41 -0500

    fix: repair main view styles.

--- src/public/pages/home.component.vue
+++ src/public/pages/home.component.vue
@@ -1,125 +1,153 @@
-<template>
-  <div class="home-container">
-    <h1>AquaEngine - Gestión de Inventario de Máquinas de Pesca</h1>
-    <div class="feature-grid">
-      <router-link to="/inventory" class="feature-item">
-        <pv-card class="feature-card">
-          <template #content>
-            <div class="feature-content">
-              <img src="../../assets/images/inventory.svg" alt="inventory-icon" class="feature-icon" />
-              <span>Inventory</span>
-            </div>
-          </template>
-        </pv-card>
-      </router-link>
-
-      <router-link to="/invoicing" class="feature-item">
-        <pv-card class="feature-card">
-          <template #content>
-            <div class="feature-content">
-              <img src="../../assets/images/invoicing.svg" alt="invoicing-icon" class="feature-icon" />
-              <span>Invoicing</span>
-            </div>
-          </template>
-        </pv-card>
-      </router-link>
-
-      <router-link to="/monitoring" class="feature-item">
-        <pv-card class="feature-card">
-          <template #content>
-            <div class="feature-content">
-              <img src="../../assets/images/monitoring.svg" alt="monitoring-icon" class="feature-icon" />
-              <span>Equipment Monitoring</span>
-            </div>
-          </template>
-        </pv-card>
-      </router-link>
-
-      <router-link to="#" class="feature-item">
-        <pv-card class="feature-card">
-          <template #content>
-            <div class="feature-content">
-              <img src="../../assets/images/machinery.svg" alt="machinery-icon" class="feature-icon" />
-              <span>Ordering Machine</span>
-            </div>
-          </template>
-        </pv-card>
-      </router-link>
-
-      <router-link to="/invoice-history" class="feature-item">
-        <pv-card class="feature-card">
-          <template #content>
-            <div class="feature-content">
-              <img src="../../assets/images/history.svg" alt="history-icon" class="feature-icon" />
-              <span>Historial de Facturas</span>
-            </div>
-          </template>
-        </pv-card>
-      </router-link>
-
-    </div>
-  </div>
-
-</template>
-
 <script>
+import { ProfileService } from "../../control/services/profile.service.js";
+import Profile from "../../control/models/profile.entity.js";
+
 export default {
-  name: 'HomeComponent',
+  name: "home-view",
+
+  data() {
+    return {
+      profileService: null,
+      profile: new Profile({}),
+    }
+  },
+
+  //#region Methods
+  methods: {
+    emitProfileSelected(profile) {
+      this.$emit('profile-selected', profile);
+    },
+
+    _buildProfileFromResponse(response) {
+      return new Profile({
+        id: response.id,
+        name: response.name,
+        email: response.email,
+        ruc: response.ruc,
+        inventory: response.inventory
+      });
+    },
+
+    _getProfileById(id) {
+      this.profileService.getById(id)
+          .then(response => {
+            this.profile = this._buildProfileFromResponse(response.data);
+            this.emitProfileSelected(this.profile);
+
+            localStorage.setItem('profile', JSON.stringify(this.profile));
+          })
+          .catch(error => {
+            console.error(error);
+          });
+    },
+
+
+  },
+  //#endregion
+
+  //#region Lifecycle Hooks
+  created() {
+    this.profileService = new ProfileService();
+    this._getProfileById(2);
+  }
+  //#endregion
 }
 </script>
 
+<template>
+  <section class="w-full lg:-mt-8">
+
+    <div class="container home pb-8 pt-5">
+
+      <h2 class="home__title title mb-5">Select your service</h2>
+
+      <div class="home__features features">
+
+        <router-link to="/inventory" class="feature__item">
+          <pv-card class="feature__card">
+            <template #content>
+              <div class="feature__content">
+                <img src="../../assets/images/inventory.svg" alt="inventory-icon" class="feature__icon"/>
+                <span>Inventory</span>
+              </div>
+            </template>
+          </pv-card>
+        </router-link>
+
+        <router-link to="#" class="feature__item">
+          <pv-card class="feature__card">
+            <template #content>
+              <div class="feature__content">
+                <img src="../../assets/images/invoicing.svg" alt="invoicing-icon" class="feature__icon"/>
+                <span>Invoicing</span>
+              </div>
+            </template>
+          </pv-card>
+        </router-link>
+
+        <router-link to="/monitoring" class="feature__item">
+          <pv-card class="feature__card">
+            <template #content>
+              <div class="feature__content">
+                <img src="../../assets/images/monitoring.svg" alt="monitoring-icon" class="feature__icon"/>
+                <span>Equipment Monitoring</span>
+              </div>
+            </template>
+          </pv-card>
+        </router-link>
+
+        <router-link to="#" class="feature__item">
+          <pv-card class="feature__card">
+            <template #content>
+              <div class="feature__content">
+                <img src="../../assets/images/machinery.svg" alt="machinery-icon" class="feature__icon"/>
+                <span>Ordering Machine</span>
+              </div>
+            </template>
+          </pv-card>
+        </router-link>
+
+        <router-link to="/invoice-history" class="feature__item">
+          <pv-card class="feature__card">
+            <template #content>
+              <div class="feature__content">
+                <img src="../../assets/images/history.svg" alt="history-icon" class="feature__icon" />
+                <span>Historial de Facturas</span>
+              </div>
+            </template>
+          </pv-card>
+        </router-link>
+
+      </div>
+    </div>
+  </section>
+
+
+
+</template>
+
+
+
 <style scoped>
-.home-container {
-  padding: 2rem;
+.features {
+  @apply grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-16;
 }
 
-h1 {
-  color: #6495ED;
-  margin-bottom: 2rem;
-  text-align: center;
+.feature__item {
+  @apply w-full col-span-1;
 }
 
-.feature-grid {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(2, 1fr);
-  gap: 2rem;
-  max-width: 800px;
-  margin: 0 auto;
+.feature__card {
+  @apply lg:h-[100px] justify-center;
+  box-shadow: 0 0 10px 6px rgba(0, 0, 0, 0.1);
 }
 
-.feature-item {
-  text-decoration: none;
+.feature__content {
+  @apply flex flex-row items-center gap-10 w-full h-full;
 }
 
-.feature-card {
-  background-color: #2a2a2a;
-  transition: transform 0.3s ease;
-  height: 100%;
+.feature__icon {
+  @apply w-10 h-10;
 }
 
-.feature-card:hover {
-  transform: translateY(-5px);
-}
-
-.feature-content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 1rem;
-  height: 100%;
-}
-
-.feature-icon {
-  width: 64px;
-  height: 64px;
-  margin-bottom: 1rem;
-}
-
-span {
-  color: #ffffff;
-  font-size: 1.2rem;
-  text-align: center;
-}
 </style>
