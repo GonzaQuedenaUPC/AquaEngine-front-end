23980500e4aff4589ae6b7b89a43964a848aa9e2
OmBRz <omarbrrcl@gmail.com>
2024-10-31 02:49:41 -0500

    chore: added path route for order detail view.

--- src/router/index.js
+++ src/router/index.js
@@ -10,13 +10,15 @@ import InventoryViewComponent from '../control/pages/inventory-view.component.vu
 import MonitoringViewComponent from "../analytics/pages/monitoring-view.component.vue";
 import MaintenanceViewComponent from "../analytics/pages/maintenance-view.component.vue";
 import OrderingMachineryViewComponent from "../planning/pages/ordering-machinery-view.component.vue";
+import OrderDetailViewComponent from "../planning/pages/order-detail-view.component.vue";
 
 const routes = [
     { path: '/inventory', name: 'inventory', component: InventoryViewComponent, meta: { title: 'Inventory'}},
-    {path:'/monitoring', name:'monitoring', component: MonitoringViewComponent, meta: {title: 'Monitoring'}},
+    { path:'/monitoring', name:'monitoring', component: MonitoringViewComponent, meta: {title: 'Monitoring'}},
     { path:'/ordering-machinery', mame:"ordering", component: OrderingMachineryViewComponent, meta: {title: 'Ordering Machinery'}},
+    { path: '/order-details', name: 'order-details', component: OrderDetailViewComponent, meta: {title: 'Order Details'}},
     { path: '/home', name: 'home', component: HomeViewComponent, meta: { title: 'Home'}},
-    {path: '/monitoring/:id/maintenance', name: 'maintenance-view', component: MaintenanceViewComponent, props: true},
+    { path: '/monitoring/:id/maintenance', name: 'maintenance-view', component: MaintenanceViewComponent, props: true},
     { path: '/', redirect: '/home'}
 ]
 
