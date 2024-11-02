e9908894d0940336365b4f1f99d1aef56ffe92b4
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-02 11:54:58 -0500

    fix(router): update import path of invoice history component.

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,8 @@ import HomeViewComponent from "../public/pages/home.component.vue";
 import InventoryViewComponent from "../control/pages/inventory-view.component.vue";
 import MonitoringViewComponent from "../analytics/pages/monitoring-view.component.vue";
 import MaintenanceViewComponent from "../analytics/pages/maintenance-view.component.vue";
-import InvoicingViewComponent from "../sales/components/Invoicing.component.vue";
-import InvoiceHistoryViewComponent from "../sales/components/invoice-history.vue";
+import InvoicingViewComponent from "../sales/components/invoicing.component.vue";
+import InvoiceHistoryViewComponent from "../sales/components/invoice-history.component.vue";
 
 const routes = [
     { path: '/inventory', name: 'inventory', component: InventoryViewComponent, meta: { title: 'Inventario' }},
