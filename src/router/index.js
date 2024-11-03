3488afd1c2943f0617965fdf8dbcf2aa3114d67b
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-02 19:35:31 -0500

    feat(router): add routes for planning features.

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import MonitoringViewComponent from "../analytics/pages/monitoring-view.componen
 import MaintenanceViewComponent from "../analytics/pages/maintenance-view.component.vue";
 import InvoicingViewComponent from "../sales/components/invoicing.component.vue";
 import InvoiceHistoryViewComponent from "../sales/components/invoice-history.component.vue";
+import OrderingMachineryViewComponent from "../planning/pages/ordering-machinery-view.component.vue";
+import OrderDetailViewComponent from "../planning/pages/order-detail-view.component.vue";
 
 const routes = [
     { path: '/inventory', name: 'inventory', component: InventoryViewComponent, meta: { title: 'Inventario' }},
@@ -14,6 +16,8 @@ const routes = [
     { path: '/monitoring/:id/maintenance', name: 'maintenance-view', component: MaintenanceViewComponent, props: true },
     { path: '/invoicing', name: 'invoicing', component: InvoicingViewComponent, meta: { title: 'Facturación' }},
     { path: '/invoice-history', name: 'invoice-history', component: InvoiceHistoryViewComponent, meta: { title: 'Historial de Facturas' }},
+    { path: '/ordering-machinery', name:'ordering-machinery', component: OrderingMachineryViewComponent, meta: { title: 'Orden de Maquinaria' }},
+    { path: '/orden-detail', name: 'orden-detail',  component: OrderDetailViewComponent, meta: { title: 'Detalle de orden'}},
     { path: '/', redirect: '/home' }
 ];
 
