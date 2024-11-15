ec505d7a99c0c28b3b01f51ee5eab080cf3c4460
teocchiii <u202211212@upc.edu.pe>
2024-11-15 00:59:54 -0500

    feat(iam): added the pages to allocate the components.

---
+++ src/iam/pages/sign-in-view.component.vue
@@ -0,0 +1,16 @@
+<script>
+import SignIn from "../components/sign-in.component.vue";
+
+export default {
+  name: "sign-in-view",
+  components: {SignIn}
+}
+</script>
+
+<template>
+<sign-in></sign-in>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/iam/pages/sign-up-view.component.vue
@@ -0,0 +1,18 @@
+<script>
+import SignUp from "../components/sign-up.component.vue";
+
+export default {
+  name: "sign-up-view",
+  components: {SignUp}
+}
+</script>
+
+<template>
+
+</template>
+
+<sign-up></sign-up>
+
+<style scoped>
+
+</style>
