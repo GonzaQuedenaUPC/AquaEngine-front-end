c3985e6a66542ad175a9863b7f4d26cca30282d7
teocchiii <u202211212@upc.edu.pe>
2024-11-16 02:38:11 -0500

    feat(monitoring): changed the monitoring service to support a form to post data.

--- src/analytics/services/monitoring.service.js
+++ src/analytics/services/monitoring.service.js
@@ -9,18 +9,27 @@ export class MonitoringService {
         console.log(this._resourceEndpoint);
         console.log('get all');
         return http.get(this._resourceEndpoint);
-
     }
+
     getById(id){
         return http.get(`${this._resourceEndpoint}/${id}`);
     }
+
     getMaintenance(){
         return this.getAll().then(response => {
             return response.data.map(item => ({
-                maintenance:item.maintenance
+                maintenance: item.maintenance
             }));
+        });
+    }
 
+    async create(newMonitoring) {
+        const response = await http.post(this._resourceEndpoint, newMonitoring);
+
+        if (!response.status === 201) {
+            throw new Error('Failed to create monitoring data');
         }
-        );
+
+        return response.data;
     }
 }
